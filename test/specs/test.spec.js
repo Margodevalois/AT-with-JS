aaefec49c9d5aa1577212e6c686e7bd72e035476
Margarita <margodevalois@gmail.com>
2023-10-06 12:26:18 +0200

    Add different assertions to tests

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -5,24 +5,36 @@ describe("HOME TASK _Introduction to WebdriverIO", () => {
       const pageTitle = await browser.getTitle();
       expect(pageTitle).toEqual("Services | EPAM");
     });
-    
+
+    it("should have 5 parts of top navigation", async () => {
+      const topNav = await $('ul.top-navigation__row');
+      expect(topNav).toHaveChildren(5);
+    });
+
+    it('should change cursor to pointer on hover', async () => {
+      const elementToHover = await $('a.top-navigation__item-link[href="/about"]');
+      await elementToHover.moveTo();
+      const cursorProperty = await elementToHover.getCSSProperty('cursor');
+      expect(cursorProperty.value).toBe('pointer');
+    });
+      
     it("should have a 'Footer' section", async () => {
       const sectionExists = await $('//div[@class="footer section"]').isExisting(); // XPath by attributes
       expect(sectionExists).toBe(true);
     });
-    
-    it("should have a 'social' part in footer", async () => {
-      const sectionExists = await $('//footer//div[@class="social"]').isExisting(); // XPath with child element of any level by attributes
-      expect(sectionExists).toBe(true);
+
+    it("should have text 'SOCIAL' in footer section", async () => {
+      const footerSection = await $('//footer//div[@class="social"]'); // XPath with child element of any level by attributes
+      expect(footerSection).toHaveText('SOCIAL');
     });
     
-    it("should have an image", async () => {
-      const imageExists = await $('img[src="/content/dam/epam/services/H_DChart_BG.gif"]').isExisting(); // CSS selector with attribute
-      expect(imageExists).toBe(true);
+    it("should have a dark-mode class for image", async () => {
+      const imageChartBG = await $('img[src="/content/dam/epam/services/H_DChart_BG.gif"]'); // CSS selector with attribute
+      expect(imageChartBG).toHaveAttr('class', 'responsive-image__image_dark-mode');
     });
     
-    it("should have a 'Strategy' button", async () => {
-      const buttonExists = await $('li:nth-child(1) span.button__content--desktop').isExisting(); // CSS Selector with Class and Child Selector
-      expect(buttonExists).toBe(true);
+    it("should have clickable 'Strategy' button", async () => {
+      const buttonStrategy = await $('li:nth-child(1) span.button__content--desktop'); // CSS Selector with Class and Child Selector
+      expect(buttonStrategy).toBeClickable()
     });
 });
